44422fe17b5357f93511cc979c85f5b1f8ba8989
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-09 14:48:16 +0800

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+import './globals.css';
+
+export default function DashboardLayout({
+                                            children,
+                                        }: {
+    children: React.ReactNode
+}) {
+    return (
+        <html lang="en">
+            <body>
+                <main>{children}</main>
+            </body>
+        </html>
+    )
+}
---
+++ app/page.tsx
@@ -0,0 +1,20 @@
+export default function Page() {
+    return (
+        <>
+            <div className="container">
+                <form action="">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <input type="submit"/>
+                </form>
+            </div>
+        </>
+    )
+}
