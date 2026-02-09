5b27f75019ed06e39d2e0f3e57d03164315db923
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-09 15:15:14 +0800

    images

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,20 @@
-
-
 export default function Page() {
     return (
         <>
             <div className="container">
                 <img src="/dudu-hearts.gif" alt=""/>
                 <form action="">
-                    <button>1</button>
-                    <button>2</button>
-                    <button>3</button>
-                    <button>4</button>
-                    <button>5</button>
-                    <button>6</button>
-                    <button>7</button>
-                    <button>8</button>
-                    <button>9</button>
+
+                    <button type="button">1</button>
+                    <button type="button">2</button>
+                    <button type="button">3</button>
+                    <button type="button">4</button>
+                    <button type="button">5</button>
+                    <button type="button">6</button>
+                    <button type="button">7</button>
+                    <button type="button">8</button>
+                    <button type="button">9</button>
+                    <button type="button">0</button>
                     <input type="submit"/>
                 </form>
             </div>
