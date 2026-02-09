687d84819977d9b5b395484db2bd30923e7225cf
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-09 15:46:20 +0800

    viewport

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,13 @@
 import './globals.css';
+import React from "react";
+
+export const viewport = {
+    width: 'device-width',
+    initialScale: 1,
+    maximumScale: 1,
+    userScalable: false,
+    themeColor: '#000000',
+}
 
 export default function DashboardLayout({
                                             children,
