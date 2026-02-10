484949721c7db1aa7008d11a9d59d5854bdf0ce1
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-10 13:23:16 +0800

    script

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
 import './globals.css';
 import React from "react";
+// 1. Import Parisienne from Google Fonts
+import {Kaushan_Script, Parisienne} from 'next/font/google';
+
+// 2. Configure the font
+const parisienne = Kaushan_Script({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+});
 
 export const viewport = {
     width: 'device-width',
@@ -16,11 +25,12 @@ export default function DashboardLayout({
 }) {
     return (
         <html lang="en">
-            <body>
-                <div className="container">
-                    <main>{children}</main>
-                </div>
-            </body>
+        {/* We removed the <link> tags because next/font handles it now! */}
+        <body className={parisienne.className}>
+        <div className="container">
+            <main>{children}</main>
+        </div>
+        </body>
         </html>
     )
 }
