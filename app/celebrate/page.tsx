c457afdace71544b28d2a7783543b09c29c2d41a
tedeidenchavez621 <tedeidenchavez621@gmail.com>
2026-02-12 17:03:46 +0800

    fix

--- app/celebrate/page.tsx
+++ app/celebrate/page.tsx
@@ -1,16 +1,17 @@
 'use client';
 import React from 'react'
+import Link from 'next/link'
 
 const Celebrate = () => {
     return (
         <>
             <img src="/dudu-happy-2.gif" alt="happy dudu"/>
             <h2>YEY! I love you 💖💗🥰💞</h2>
-            <form action="/plan" method="POST">
-                <button className="Submit">
+            <Link href="/plan">
+                <button className="submit">
                     CONTINUE
                 </button>
-            </form>
+            </Link>
 
         </>
     )
